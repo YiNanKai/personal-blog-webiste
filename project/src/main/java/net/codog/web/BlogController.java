d5ba08636b723011ad45264d8b1869bdcdfc31cc
王文涵 <wenhanwang@creditease.cn>
2016-11-11 11:38:35 +0800

    规范化了代码结构 Signed-off-by: 王文涵 <wenhanwang@creditease.cn>

--- project/src/main/java/net/codog/web/BlogController.java
+++ project/src/main/java/net/codog/web/BlogController.java
@@ -11,11 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-/**
-* @author 王文涵 
-* @date 2016年10月24日
-* <p>Description: 博客的controller层，用来处理博客界面</p>
- */
+
 /**
 * @author 王文涵 
 * @date 2016年10月24日
