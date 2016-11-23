95018502136b276a28898861bb89f1f39a7b713e
王文涵 <wenhanwang@creditease.cn>
2016-11-23 15:55:08 +0800

    加入到java项目中，进行页面组合实验 Signed-off-by: 王文涵 <wenhanwang@creditease.cn>

---
+++ project/src/main/resources/public/js/mainhtml.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+	
+    $(function() {
+        $('#nav-accordion li').click(function() {
+            var $this = $(this),
+            _clickTab = $this.find('a').attr('target');
+            $.get(_clickTab,
+            function(data) {
+				console.log(data);
+                $("#iframe").html(data);
+            });
+        });
+    });
+    
+});
