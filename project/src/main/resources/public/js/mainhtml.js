952fa3b90d072ece8d8430a1b257fa617e7431da
王文涵 <wenhanwang@creditease.cn>
2016-11-24 17:48:18 +0800

    修改了点击出现页面消失bug的情况 Signed-off-by: 王文涵 <wenhanwang@creditease.cn>

--- project/src/main/resources/public/js/mainhtml.js
+++ project/src/main/resources/public/js/mainhtml.js
@@ -4,6 +4,10 @@
         $('#nav-accordion li').click(function() {
             var $this = $(this),
             _clickTab = $this.find('a').attr('target');
+            //console.log(_clickTab);
+            if(_clickTab === "javascript:;"){
+            	return;
+            }
             $("#realcontent object").attr("data",_clickTab);
             /*$.get(_clickTab,
             function(data) {
