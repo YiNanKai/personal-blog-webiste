9630e1422f63a00955b571f4ae0a8e4bf9c7f800
王文涵 <wenhanwang@creditease.cn>
2016-12-26 17:24:57 +0800

    Signed-off-by: 王文æ¶? <wenhanwang@creditease.cn>

--- project/src/main/java/net/codog/ProjectApplication.java
+++ project/src/main/java/net/codog/ProjectApplication.java
@@ -2,8 +2,10 @@ package net.codog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProjectApplication {
 
 	public static void main(String[] args) {
