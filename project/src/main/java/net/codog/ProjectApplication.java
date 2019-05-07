5b1444f21ff710566ca485da288c7f0bf5460246
justforuse <allenforuse@gmail.com>
2019-05-08 00:45:33 +0800

    add https support

--- project/src/main/java/net/codog/ProjectApplication.java
+++ project/src/main/java/net/codog/ProjectApplication.java
@@ -7,15 +7,32 @@ import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
-public class ProjectApplication {
+public class ProjectApplication implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-	
+
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 
@@ -30,4 +47,40 @@ public class ProjectApplication {
 			container.addErrorPages(error401Page, error404Page, error500Page);
 		});
 	}
+
+		@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	//springboot版本不同可能下面的类名会不同或者类的包路径会不同
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint constraint = new SecurityConstraint();
+				constraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				constraint.addCollection(collection);
+				context.addConstraint(constraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//监听http的端口号
+		connector.setPort(80);
+		connector.setSecure(false);
+		//监听到http的端口号后转向到的https的端口号
+        System.out.println("监听到了80端口");
+		connector.setRedirectPort(443);//这里的端口写成和配置文件一样的端口就Ok
+		return connector;
+	}
+
+@Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(443);
+    }
 }
